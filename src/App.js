8877de86c216ad92fee3f9d206f775fc5533b785
Alanne Lucena de Brito <alanne.lucena@flowef.com>
2022-05-22 11:18:22 -0300

    frist commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Menu from './componentes/navbar/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <section Id="top">
+    <Menu />
+
+
+    </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/navbar/navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Navbar, Nav} from 'react-bootstrap';
+import Logo from '../imagens/Suddath.webp'
+import "./navbar.css"
+import {Row, Col, Container} from 'react-bootstrap';
+
+function Menu() {
+  return (
+      <>
+      <Container>
+  <Row>
+    <Col>
+    <Navbar bg="light" expand="lg" lg={6}>
+  <Container className='containeresquerda'>
+    <Navbar.Brand href="#home">Portifolio</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Contato</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    </Col>
+    <Col>
+    <div className='logocontainer' lg={6}>
+            <img className='Logoimage' src= {Logo} alt="logo" />
+        </div>
+    </Col>
+  </Row>
+
+    
+        
+        </Container>
+        </>
+  )
+}
+
+export default Menu
+
+
