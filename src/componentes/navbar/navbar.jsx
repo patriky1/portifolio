87b1da715b71ce7f0931c54041948f892a68593b
patriky1 <patrikybrito@gmail.com>
2022-05-23 20:25:47 -0300

    navbar

--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
 import Logo from "../imagens/Suddath.jpg";
 import "./navbar.css";
 import { Row, Col, Container } from "react-bootstrap";
@@ -7,28 +7,33 @@ import { Row, Col, Container } from "react-bootstrap";
 function Menu() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <Navbar expand="lg" lg={6}>
+      <Container fluid className="bgcolorintem">
+        <Row className="colapse" lg={6}>
+          {/* <Col>
+            <Navbar fixed="top" className="corfundo">
+              <img className="Logoimage" src={Logo} alt="logo" />
               <Container className="containeresquerda">
-                <Navbar.Brand href="#home">Portifolio</Navbar.Brand>
+                <Navbar.Brand href="#home"className="flexcenter"/>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
+                <Navbar.Collapse id="basic-navbar-nav" >
+                  <Nav>
+                    <Nav.Link href="#portifolio">Portifolio</Nav.Link>
                     <Nav.Link href="#home">Inicio</Nav.Link>
-                    <Nav.Link href="mailto: patrikybrito@gmail.com">
-                      Contato
-                    </Nav.Link>
+                    <Nav.Link href="mailto: patrikybrito@gmail.com">Contato</Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
               </Container>
             </Navbar>
+          </Col> */}
+
+
+          <Col className="itens" lg={6} ><a className="item" href="#portfolio">Portifolio</a>
+          <a className="item" href="#home">Home</a>
+          <a className="item" href="#contato">Contato</a>
+ 
           </Col>
-          <Col>
-            <div className="logocontainer" lg={6}>
-              <img className="Logoimage" src={Logo} alt="logo" />
-            </div>
+          <Col  lg={6}>
+          <div className="logocontainer"><img className="logoimage" src={Logo} alt="logo" /></div>
           </Col>
         </Row>
       </Container>
