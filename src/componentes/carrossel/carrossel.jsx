d5f229ba3fe3b2a10eaab80bd9b566621d76b4bf
patriky1 <patrikybrito@gmail.com>
2022-05-24 20:18:27 -0300

    att carrosssel

--- src/componentes/carrossel/carrossel.jsx
+++ src/componentes/carrossel/carrossel.jsx
@@ -2,6 +2,10 @@ import Carousel from "nuka-carousel";
 import Imagem1 from "../imagens/imc1.png"
 import Imagem2 from "../imagens/imc2.png"
 import Imagem3 from "../imagens/imc3.png"
+import Imagem4 from "../imagens/imc4.png"
+import Imagem5 from "../imagens/imc5.png"
+import Imagem6 from "../imagens/imc6.png"
+import Imagem7 from "../imagens/imc7.png"
 
 
 import React from 'react'
@@ -18,6 +22,10 @@ function Carrossel() {
     <img src={Imagem1} height="400px" width="200px" alt="...."/>
     <img src={Imagem2} height="400px" width="200px" alt="...." />
     <img src={Imagem3} height="400px" width="200px" alt="...." />
+    <img src={Imagem4} height="400px" width="200px" alt="...." />
+    <img src={Imagem5} height="400px" width="200px" alt="...." />
+    <img src={Imagem6} height="400px" width="200px" alt="...." />
+    <img src={Imagem7} height="400px" width="200px" alt="...." />
   </Carousel>
     
     </>
