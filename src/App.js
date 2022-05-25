9e0395a2806fd0be64e00ce67257fcd2ce09d606
patriky1 <patrikybrito@gmail.com>
2022-05-24 22:11:56 -0300

    scroll top

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Menu from "./componentes/navbar/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { GridCarousel, GridEducation, GridPerfil, GridSkills } from "./componentes/grid/grid1";
 import Footer from "./componentes/footer/footer";
+import ScrollToTop from "./componentes/scroll/scrollToTop";
 
 function App() {
   return (
     <>
+    <ScrollToTop/>
       <section id="home">
         <Menu />
         <GridPerfil />
